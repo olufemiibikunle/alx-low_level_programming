e82b082597079313c6bcf628fecd8d199f66a76f
Olufemi Ibikunle <ibikunleolufemi@gmail.com>
2022-09-08 09:18:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,4 @@
 #include <stdio.h>
-/**
- *  * Description: main - Prints out a string to stdout.
- *   * Return: 0 if success.
- *    */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-#include <stdio.h>
 
 /**
  *  * main - Entry point
@@ -16,6 +7,6 @@ int main(void)
  *     */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
