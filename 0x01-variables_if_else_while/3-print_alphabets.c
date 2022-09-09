2ce76d91e26c4f875e518f16aeb38503f2d885e2
Olufemi Ibikunle <ibikunleolufemi@gmail.com>
2022-09-09 15:17:36 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,18 +6,20 @@
 */
 int main(void)
 {
-char c = 'a';
-char d = 'A';
-		
-while (c <= 'z')
-{
-putchar(c);
-c++;										}					
-while (d <= 'Z')
-{
-putchar(d);
-d++;
-putchar('\n');
-return (0);
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+	putchar(c);
+	c++;
+											}
+	while (d <= 'Z')
+	{
+	putchar(d);
+	d++;
+	}
+	putchar('\n');
+	return (0);
 
 }
