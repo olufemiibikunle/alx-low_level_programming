a4d1ddc9abdb0f21cade57d53bef937dfc335dc4
Olufemi Ibikunle <ibikunleolufemi@gmail.com>
2022-09-13 08:50:16 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-		char *sh = "Holberton";
+char *sh = "_putchar";
 
-			while (*sh)
-					{
-								_putchar(*sh);
-										sh++;
-											}
-				_putchar('\n');
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
 
-					return (0);
+return (0);
 }
