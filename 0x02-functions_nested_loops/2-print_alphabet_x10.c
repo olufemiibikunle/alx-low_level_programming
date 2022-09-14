3f2c24ff406febb2f508d8956f9b874cc3a92546
Olufemi Ibikunle <ibikunleolufemi@gmail.com>
2022-09-14 15:32:08 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,11 @@
-#include "main.h"                                                                                                                                               
-                                                                                                                                                                
-/**                                                                                                                                                             * print_alphabet_x10 -Prints the alphabet 10 times.                            
+#include "main.h"
+/**
+* print_alphabet_x10 -Prints the alphabet 10 times.
 *
 * Return: Always 0 (Success)
 */
 void print_alphabet_x10(void)
 {
-
 int a, b;
 
 for (a = 0; a <= 0; a++)
