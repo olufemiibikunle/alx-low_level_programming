aaaf77d3737fd1ce0a8a06d645209f0b045eefc2
Olufemi Ibikunle <ibikunleolufemi@gmail.com>
2022-09-15 16:33:22 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,80 @@
 #include "main.h"
+
 /**
- *  * print_number - prints an integer
- *   * @n:.input integer parameter
- *    *
- *     *
- *      */
+ *  * print_number - prints a number
+ *   * @n: number to print
+ *    */
 void print_number(int n)
 {
-		unsigned int i = n;
+		int i;
 
 			if (n < 0)
 					{
-								_putchar(45);
-										i = -i;
+								_putchar('-');
+										for (i = 0; i < getlen(n); i++)
+														_putchar('0' + -getnum(n, i));
 											}
-				if (i / 10)
+				else
 						{
-									print_number(i / 10);
+									for (i = 0; i < getlen(n); i++)
+													_putchar('0' + getnum(n, i));
 										}
-					_putchar(i % 10 + '0');
+}
+
+/**
+ *  * _pow - Calculates exponents
+ *   * @a: base
+ *    * @b: exponent
+ *     *
+ *      * Return: a^b
+ *       */
+int _pow(int a, int b)
+{
+		int i, ans;
+
+			if (b <= 0)
+						return (1);
+
+				ans = a;
+					for (i = 0; i < b - 1; i++)
+							{
+										ans *= a;
+											}
+
+						return (ans);
+}
+
+/**
+ *  * getnum - gets the 'index'th digit of num, starting from the left at 0
+ *   * @num: the number to extract digit from
+ *    * @index: the index of the desired digit with 0 being the leftmost digit
+ *     *
+ *      * Return: The 'index'th digit of num
+ *       */
+int getnum(int num, int index)
+{
+		int l;
+
+			l = getlen(num);
+				num = num / _pow(10, l - 1 - index);
+					return (num % 10);
+}
+
+/**
+ *  * getlen - gets the length of the number num
+ *   * @num: the number to get length of
+ *    *
+ *     * Return: length of number 'num'
+ *      */
+int getlen(int num)
+{
+		int i;
+
+			i = 1;
+				while (num > 9 || num < -9)
+						{
+									i++;
+											num /= 10;
+												}
+					return (i);
 }
