c25af5f869641bb9fdfc177c30b986633d799e97
Olufemi Ibikunle <ibikunleolufemi@gmail.com>
2022-09-15 15:43:36 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * * print_triangle - print a triangle
+ * *
+ * * @size: The charater to print
+ * */
+
+void print_triangle(int size)
+{
+		int row, col;
+
+			if (size > 0)
+					{
+								for (row = 1; row <= size; row++)
+											{
+															for (col = size - row; col > 0; col--)
+																				_putchar(' ');
+																		
+																		for (col = 0; col < row; col++)
+																							_putchar('#');
+
+																					if (row == size)
+																										continue;
+																								_putchar('\n');
+																										}
+									}
+				_putchar('\n');
+}
