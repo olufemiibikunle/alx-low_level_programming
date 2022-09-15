dd42749f03b68b15032649554e06c7b45881b40a
Olufemi Ibikunle <ibikunleolufemi@gmail.com>
2022-09-15 03:35:48 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,22 +15,23 @@ printf("%lu, %lu, ", bk1, bk2);
 for (count = 2; count < 98; count++)
 {
 if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
-{hold1 = (bk1 + bk2) / LARGEST;
-																					hold2 = (bk1 + bk2) % LARGEST;
-																								hold3 = fr1 + fr2 + hold1;
-																											fr1 = fr2, fr2 = hold3;
-																														bk1 = bk2, bk2 = hold2;
-																																	printf("%lu%010lu", fr2, bk2);
-																																			}
-													else
-																{
-																				hold2 = bk1 + bk2;
-																							bk1 = bk2, bk2 = hold2;
-																										printf("%lu", bk2);
-																												}
-															if (count != 97)
-																			printf(", ");
-																}
-							printf("\n");
-								return (0);
+{
+hold1 = (bk1 + bk2) / LARGEST;
+hold2 = (bk1 + bk2) % LARGEST;
+hold3 = fr1 + fr2 + hold1;
+fr1 = fr2, fr2 = hold3;
+bk1 = bk2, bk2 = hold2;
+printf("%lu%010lu", fr2, bk2);
+}
+else
+{
+hold2 = bk1 + bk2;
+bk1 = bk2, bk2 = hold2;
+printf("%lu", bk2);
+}
+if (count != 97)
+printf(", ");
+}
+printf("\n");
+return (0);
 }
