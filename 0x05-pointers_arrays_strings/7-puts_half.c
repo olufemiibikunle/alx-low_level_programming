e416749861726abdc5f0bd8923df976d1eaa7138
Olufemi Ibikunle <ibikunleolufemi@gmail.com>
2022-09-19 12:54:11 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,20 +1,25 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- *  * puts_half - check the code for Holberton School students.
- *   * @str: pointer int type
- *    * Return: Always 0.
- *     */
+ *  * puts_half - Entry point
+ *   * Desc: Entry
+ *    *@str: pointer
+ *     * Return: Always 0 (Success)
+ *      */
 void puts_half(char *str)
 {
-	int i;
-	for (i = 0; *str != '\0'; i++)
-		str++;
+		int i;
+			int k;
 
-	i = i / 2;
-
-	for (i = i; i != 0; i--)
-		_putchar(*(str - i));
-
-	_putchar('\n');
+				for (i = 0 ; str[i] != '\0' ; i++)
+						{
+								}
+					i--;
+						k = i % 2;
+							i = (i - k) / 2;
+								for (i = i + 1; str[i] != '\0'; i++)
+										{
+													_putchar(str[i]);
+														}
+									_putchar('\n');
 }
