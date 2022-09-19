a24564cedd71aa4fcae1e1cf9710b7ea0a2bf5c9
Olufemi Ibikunle <ibikunleolufemi@gmail.com>
2022-09-19 12:07:54 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 /**
- *  * puts2 - print reverse characters.
- *   * @str: validate the character
- *    * Return: Always 0.
- *     */
+ *   * puts2 - Prints every other character of a string
+ *     * @str: The string to be treated
+ *       *
+ *         * Return: void
+ *           */
 void puts2(char *str)
 {
-		int i = 0;
+		int i;
+			int j = 0;
 
-			while (str[i] != '\0')
-					{
-								if (i % 2 == 0)
-											{
-														_putchar(str[i]);
-																}
-										i++;
+				while (str[j] != '\0')
+						{
+									j++;
+										}
+
+					for (i = 0; i < j; i += 2)
+							{
+										_putchar(str[i]);
 											}
-				_putchar('\n');
+
+						_putchar('\n');
 }
