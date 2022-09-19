d96339ff02fcb96cf15788868fea0d125e4a87d8
Olufemi Ibikunle <ibikunleolufemi@gmail.com>
2022-09-19 13:12:39 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -16,19 +16,3 @@ int main(void)
 				_puts(str);
 					return (0);
 }
-/**
- *  * _puts - print a string
- *   * @str:pointer char
- *    * return:void
- *     */
-void _puts(char *str)
-{
-		int i;
-
-			for (i = 0; str[i] != '\0'; i++)
-					{
-								_putchar(str[i]);
-									}
-			_putchar('\n');
-			return;
-}
