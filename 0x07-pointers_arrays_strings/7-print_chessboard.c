51d07369d475c369046b702b37facbf323b93f9d
Olufemi Ibikunle <ibikunleolufemi@gmail.com>
2022-09-27 14:43:56 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,22 @@
 #include "main.h"
-/**
- * print_chessboard - Entry point
- * @a: array
- * Return: Always 0 (Success)
- */
-void print_chessboard(char (*a)[8])
-{
-		int i;
-			int j;
+#include <stdio.h>
 
-				for (i = 0; i < 8; i++)
-						{
-									for (j = 0; j < 8; j++)
-													_putchar(a[i][j]);
-											_putchar('\n');
-												}
-}
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	    char board[8][8] = {
+		            {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+			            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+				            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+					            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+						            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+							            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+								            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+									            {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+										        };
+	        print_chessboard(board);
+		    return (0);
