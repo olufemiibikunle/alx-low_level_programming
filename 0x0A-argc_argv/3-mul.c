d34a768b216a4f8c5bd59d3ebff7f046474081ed
Olufemi Ibikunle <ibikunleolufemi@gmail.com>
2022-10-01 03:14:56 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,22 +11,21 @@
 
 int main(int argc, char *argv[])
 {
-		int i, j;
+int i, j;
 
-			if (argc == 1 || argc == 2)
-					{
-								printf("Error\n");
-										return (1);
-											}
-				else
-						{
-									j = 1;
+if (argc == 1 || argc == 2)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+j = 1;
 
-											for (i = 1; i < 3; i++)
-														j *= atoi(argv[i]);
+for (i = 1; i < 3; i++)
+j *= atoi(argv[i]);
 
-													printf("%d\n", j);
-														}
-
-					return (0);
+printf("%d\n", j);
+}
+return (0);
 }
