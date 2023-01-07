77cb50365549e00344513b390f8c9544b36336f9
olufemiibikunle <ibikunleolufemi@gmail.com>
2023-01-07 12:56:45 -0800

    tasks

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,12 +1,14 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
+#ifndef _HASH_TABLE_H_
+#define _HASH_TABLE_H_
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+/*********** STRUCTS AND LISTS *******/
 
 /**
  * struct hash_node_s - Node of a hash table
+ *
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
@@ -21,6 +23,7 @@ typedef struct hash_node_s
 
 /**
  * struct hash_table_s - Hash table data structure
+ *
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
@@ -32,6 +35,8 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/****** Hash functions *******/
+
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -40,8 +45,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
+
 /**
  * struct shash_node_s - Node of a sorted hash table
+ *
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
@@ -60,6 +67,7 @@ typedef struct shash_node_s
 
 /**
  * struct shash_table_s - Sorted hash table data structure
+ *
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
@@ -82,4 +90,4 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
-#endif /* HASH_TABLES_H */
+#endif
